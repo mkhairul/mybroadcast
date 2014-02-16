92e95dca5bd013a81f599c5c782bc39abac058bf
mkhairul <mkhairul@gmail.com>
2014-02-16 18:22:07 +0800

    update user presence in channel

--- app/views/chat.php
+++ app/views/chat.php
@@ -1,5 +1,16 @@
                     <div id="<?php echo $room_id; ?>" class="chat-window">
 						<h2>#lobby</h2>
+						<div class="chat-options">
+							<a href="#" class="users active"><i class="fa fa-user"></i></a>
+							<div class="users-list">
+								<ul>
+									<li><a href="#">MK</a></li>
+									<li><a href="#">MK</a></li>
+									<li><a href="#">MK</a></li>
+									<li><a href="#">MK</a></li>
+								</ul>
+							</div>
+						</div>
 						<div class="chat">
                             <!--
 							<div class="col-md-12 chat-row">
@@ -16,6 +27,7 @@
                     <script>
                         $('.chat-window').height($('#page-content').height()-$('#page-content .container').position().top)
                         var room_id = '<?php echo $room_id; ?>';
+						var user_list = [];
                         $('#'+room_id).on('keypress', '.message input', function(e){
                             var p = e.which;
                             if(p == 13)
@@ -71,6 +83,50 @@
                         var get_time = function(){
                             return moment().format('h:mmA');
                         }
+						
+						function diff(A, B) {
+							return A.filter(function (a) {
+								return B.indexOf(a) == -1;
+							});
+						}
+						
+						var update_presence = function(event_name, users)
+						{
+							var tmp_users = []
+							$.each($users, function(index, user)
+							{
+								for(var key in user)
+								{						
+									// set in a list to compare with existing users
+									tmp_users.push(key)
+								}	
+							})
+							
+							// get the existing users
+							if (user_list.length == 0) {
+								$('.users-list [data-username]').each(function(){
+									$(this).attr('data-username')
+								})
+							}
+							
+							// remove users who are not in the latest presence
+							var missing = diff(user_list, tmp_users);
+							for(var i=0; i<missing.length; i++)
+							{
+								tmp_users.splice(user_list.indexOf(missing[i]), 1);
+							}
+							
+							var new_users = diff(tmp_users, user_list);
+							for(var i=0; i<new_users.length; i++)
+							{
+								var list = $('<li/>');
+								var names = $('<a/>').attr('href', '#');
+								$(names).attr('data-username', new_users[i]).html(new_users[i]);
+								$(list).html(names);
+								$('.users-list ul').html('');
+								$('.users-list ul').append(list);
+							}
+						}
 						
 						//socket.on('update', function (data) {
 						socket.on('<?php echo $room_id; ?>', function (data) {
